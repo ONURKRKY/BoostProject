f27fdfea5df2bbaa488f022c090e9491f3003d93
onur <karakayaonur@yahoo.com>
2021-08-15 11:24:24 +0300

    fuel added

---
+++ Assets/CollisionHandler.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionHandler : MonoBehaviour
+{
+     void OnCollisionEnter(Collision other)
+   {
+
+   switch(other.gameObject.tag)
+   {
+       case"Friendly":
+       Debug.Log("friendly");
+       break;
+
+       case"Finish":
+       Debug.Log("Finish");
+       break;
+
+       case"Fuel":
+       Debug.Log("Fuel");
+       break;
+
+        default:
+        Debug.Log("you blow up");
+        break;
+
+   }
+
+   }
+}
