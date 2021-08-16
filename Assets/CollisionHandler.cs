2206ac7c0b09bfabcfd0c44a9d51d751c3a67e32
onur <karakayaonur@yahoo.com>
2021-08-16 18:36:46 +0300

    level2 added

--- Assets/CollisionHandler.cs
+++ Assets/CollisionHandler.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
@@ -14,7 +15,7 @@ public class CollisionHandler : MonoBehaviour
        break;
 
        case"Finish":
-       Debug.Log("Finish");
+       LoadNextLevel();
        break;
 
        case"Fuel":
@@ -22,10 +23,29 @@ public class CollisionHandler : MonoBehaviour
        break;
 
         default:
-        Debug.Log("you blow up");
+        ReloadLevel();
         break;
 
    }
 
    }
+
+void LoadNextLevel()
+{
+ int currentSceneIndex=SceneManager.GetActiveScene().buildIndex;
+ int nextSceneIndex=currentSceneIndex+1;
+
+ if(nextSceneIndex==SceneManager.sceneCountInBuildSettings)
+ {
+      nextSceneIndex=0;
+ }
+   SceneManager.LoadScene(nextSceneIndex );
+
+
+}
+void ReloadLevel()
+{
+   int currentSceneIndex=SceneManager.GetActiveScene().buildIndex;
+   SceneManager.LoadScene(currentSceneIndex);
+}
 }
