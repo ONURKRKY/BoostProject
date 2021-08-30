97b19a88f319e8f97c4e6488bdebd9b5fc5b48b6
onur <karakayaonur@yahoo.com>
2021-08-30 14:34:57 +0300

    moving obstacle

---
+++ Assets/Oscilator.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Oscilator : MonoBehaviour
+{
+    Vector3 startinPosition;
+    [SerializeField] Vector3 movementVector;
+    float movementFactor;
+    [SerializeField] float Period = 2f;
+
+
+
+    void Start()
+    {
+        startinPosition = transform.position;
+
+    }
+
+
+    void Update()
+
+    {
+        if(Period<=Mathf.Epsilon){return;}  // 0 bölme hatası
+        float cycles = Time.time / Period;      // kaç döngü var
+        const float tau = Mathf.PI * 2;    //2pi değeri sabit
+        float rawSinWave = Mathf.Sin(cycles * tau);
+        movementFactor = (rawSinWave + 1f) / 2f;
+
+        Vector3 offset = movementVector * movementFactor;
+        transform.position = startinPosition + offset;
+    }
+}
